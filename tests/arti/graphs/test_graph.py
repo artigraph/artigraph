ea93142c4706af37d85237b0d43ca0b83907e937
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-03-31 01:34:01 -0400

    Add mypy plugin with stricter Producer .__init__ and .out hints
    Fixes #25

--- tests/arti/graphs/test_graph.py
+++ tests/arti/graphs/test_graph.py
@@ -6,14 +6,12 @@ from tests.arti.dummies import A1, P1, P2
 
 
 def test_Graph() -> None:
+    # NOTE: .out() supports strict Artifact subclass mypy typing with the mypy_plugin, but Producers
+    # also support simple iteration (eg: `a, b = MyProducer(...)`).
     with Graph("test") as graph:
         graph.artifacts.a = A1()
-        graph.artifacts.b.c = P1(input_artifact=graph.artifacts.a)
-        # mypy thinks (understandably) that b.c is a Producer instance and didn't catch the casting.
-        # We may need to make a mypy plugin[1] to parse these.
-        #
-        # 1: https://github.com/replicahq/artigraph/issues/25
-        graph.artifacts.c, graph.artifacts.d = P2(input_artifact=graph.artifacts.b.c)  # type: ignore
+        graph.artifacts.b.c = P1(input_artifact=graph.artifacts.a).out()
+        graph.artifacts.c, graph.artifacts.d = P2(input_artifact=graph.artifacts.b.c).out()
 
     with pytest.raises(BoxError, match="Box is frozen"):
         graph.artifacts.a = A1()
--- tests/arti/producers/test_producer.py
+++ tests/arti/producers/test_producer.py
@@ -1,4 +1,4 @@
-from typing import Any, no_type_check
+from typing import Any
 
 import pytest
 
@@ -27,13 +27,15 @@ def test_Producer() -> None:
 def test_Producer_out() -> None:
     a1, a2, a3, a4 = A1(), A2(), A3(), A4()
     # single return Producer
-    assert isinstance(P1(input_artifact=a1).out(), A2)
     assert P1(input_artifact=a1).out(a2) is a2
+    assert isinstance(P1(input_artifact=a1).out(), A2)
     assert isinstance(list(P1(input_artifact=a1))[0], A2)
     # multi return Producer
-    assert isinstance(P2(input_artifact=a2).out(), tuple)
     assert P2(input_artifact=a2).out(a3, a4) == (a3, a4)
+    assert isinstance(P2(input_artifact=a2).out()[0], A3)
+    assert isinstance(P2(input_artifact=a2).out()[1], A4)
     assert isinstance(list(P2(input_artifact=a2))[0], A3)
+    assert isinstance(list(P2(input_artifact=a2))[1], A4)
 
 
 @pytest.mark.xfail(raises=ValueError)
@@ -54,73 +56,72 @@ def test_Producer_mutations() -> None:
     assert o2.producer is producer
 
 
-@no_type_check
 def test_Producer_bad_signature() -> None:  # noqa: C901
     # pylint: disable=function-redefined
 
     with pytest.raises(ValueError, match="Producers must implement"):
 
-        class BadProducer(Producer):
+        class BadProducer(Producer):  # type: ignore
             pass
 
     with pytest.raises(ValueError, match="must have a type hint"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact):
+            def build(self, input_artifact):  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="type hint must be an Artifact subclass"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact: str) -> tuple[A2, A3]:
+            def build(self, input_artifact: str) -> tuple[A2, A3]:  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="must not have a default"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact: A1 = A1()):
+            def build(self, input_artifact: A1 = A1()):  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="parameter must be usable as a keyword argument"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact: A1, /):
+            def build(self, input_artifact: A1, /):  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="parameter must be usable as a keyword argument"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, *args: A1):
+            def build(self, *args: A1):  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="parameter must be usable as a keyword argument"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, **kwargs: A1):
+            def build(self, **kwargs: A1):  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="return value must be an Artifact"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact: A1) -> None:
+            def build(self, input_artifact: A1) -> None:  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="A return value must be set"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact: A1):
+            def build(self, input_artifact: A1):  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="return value must be an Artifact"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact: A1) -> str:
+            def build(self, input_artifact: A1) -> str:  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="return value must be an Artifact"):
 
         class BadProducer(Producer):  # type: ignore # noqa: F811
-            def build(self, input_artifact: A1) -> tuple[A2, str]:
+            def build(self, input_artifact: A1) -> tuple[A2, str]:  # type: ignore
                 pass
 
     with pytest.raises(ValueError, match="The parameters to `map` .* must match `build`"):
@@ -139,7 +140,7 @@ def test_Producer_bad_init() -> None:
     with pytest.raises(ValueError, match="Unknown argument"):
         DummyProducer(junk=5)  # type: ignore
     with pytest.raises(ValueError, match="Missing argument"):
-        DummyProducer()
+        DummyProducer()  # type: ignore
     with pytest.raises(ValueError, match="expects an instance of"):
         DummyProducer(input_artifact=5)  # type: ignore
 
@@ -153,6 +154,5 @@ def test_Producer_bad_out() -> None:
     with pytest.raises(ValueError, match="Expected the 2nd argument to be"):
         producer.out(A2(), A2())
     output = producer.out(A2(), A3())
-    assert isinstance(output, tuple)
     with pytest.raises(ValueError, match="is produced by"):
         producer.out(*output)
