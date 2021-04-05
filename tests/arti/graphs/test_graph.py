eb735de41242d37b4742f36c2e8cc743f92e196a
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-04-05 19:06:28 -0400

    Split up graph test and add cross-graph reference

--- tests/arti/graphs/test_graph.py
+++ tests/arti/graphs/test_graph.py
@@ -8,10 +8,19 @@ from tests.arti.dummies import A1, P1, P2
 def test_Graph() -> None:
     # NOTE: .out() supports strict Artifact subclass mypy typing with the mypy_plugin, but Producers
     # also support simple iteration (eg: `a, b = MyProducer(...)`).
+    with Graph("test") as g1:
+        g1.artifacts.a = A1()
+        g1.artifacts.b.c = P1(input_artifact=g1.artifacts.a).out()
+        g1.artifacts.c, g1.artifacts.d = P2(input_artifact=g1.artifacts.b.c).out()
+
+    with Graph("test-2") as g2:
+        g2.artifacts.upstream.a = g1.artifacts.a
+
+
+def test_Graph_errors() -> None:
     with Graph("test") as graph:
         graph.artifacts.a = A1()
-        graph.artifacts.b.c = P1(input_artifact=graph.artifacts.a).out()
-        graph.artifacts.c, graph.artifacts.d = P2(input_artifact=graph.artifacts.b.c).out()
+        graph.artifacts.b = P1(input_artifact=graph.artifacts.a).out()
 
     with pytest.raises(BoxError, match="Box is frozen"):
         graph.artifacts.a = A1()
