426758ba151644ff1bea2bf752bb0d2807927ac8
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-04-18 02:48:41 -0400

    Add (u)int64 internal types

--- tests/arti/internal/test_utils.py
+++ tests/arti/internal/test_utils.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
 
-from typing import cast
+import math
+import operator as op
+from collections.abc import Callable
+from functools import partial
+from typing import Any, Union, cast
 
 import pytest
 
-from arti.internal.utils import TypedBox, class_name, classproperty, ordinal, register
+from arti.internal.utils import (
+    TypedBox,
+    _int,
+    class_name,
+    classproperty,
+    int64,
+    ordinal,
+    qname,
+    register,
+    uint64,
+)
 
 
 def test_class_name() -> None:
@@ -32,6 +46,79 @@ def test_classproperty() -> None:
     assert Test().b == "Test"
 
 
+class _I(_int):
+    pass
+
+
+@pytest.mark.parametrize(
+    ["op"],
+    (
+        (op.add,),
+        (op.and_,),
+        (op.floordiv,),
+        (op.lshift,),
+        (op.mod,),
+        (op.mul,),
+        (op.or_,),
+        (op.rshift,),
+        (op.sub,),
+        (op.xor,),
+    ),
+)
+def test__int_binary(op: Callable[..., Any]) -> None:
+    i, _i = 123, _I(123)
+    left = op(_i, i)
+    right = op(i, _i)
+    assert left == right
+    assert isinstance(left, _I)
+    assert isinstance(right, _I)
+
+
+@pytest.mark.parametrize(
+    ["op"],
+    (
+        (math.ceil,),
+        (math.floor,),
+        (math.trunc,),
+        (op.invert,),
+        (op.neg,),
+        (op.pos,),
+        (partial(round, ndigits=-1),),
+    ),
+)
+def test__int_unary(op: Callable[..., Any]) -> None:
+    output, expected = op(_I(123)), op(123)
+    assert output == expected
+    assert isinstance(output, _I)
+
+
+def test__int_repr() -> None:
+    assert repr(_I(5)) == "_I(5)"
+
+
+@pytest.mark.parametrize(
+    ["typ"], ((int64,), (uint64,)),
+)
+def test_sizedint(typ: type[Union[int64, uint64]]) -> None:
+    low, high = typ(typ._min), typ(typ._max)
+
+    assert low == typ._min
+    assert high == typ._max
+
+    with pytest.raises(ValueError):
+        low - 1
+
+    with pytest.raises(ValueError):
+        high + 1
+
+
+def test_sizedint_cast() -> None:
+    assert int64(uint64(18446744073709551611)) == int64(-5)
+    assert int64(uint64(5)) == int64(5)
+    assert uint64(int64(-5)) == uint64(18446744073709551611)
+    assert uint64(int64(5)) == uint64(5)
+
+
 def test_ordinal() -> None:
     assert ordinal(0) == "0th"
     assert ordinal(3) == "3rd"
@@ -47,6 +134,11 @@ def test_register() -> None:
     assert reg == {"x": 5, "y": 5}
     with pytest.raises(ValueError, match="is already registered"):
         register(reg, "x", 10)
+
+
+def test_qname() -> None:
+    assert qname(TypedBox) == "TypedBox"
+    assert qname(TypedBox()) == "TypedBox"
 
 
 class BaseCoord:
