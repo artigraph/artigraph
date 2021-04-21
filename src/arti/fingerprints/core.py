91966256e6a7fb1b946953768bd61b96761fd6f1
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-04-20 22:43:51 -0400

    Add base Fingerprint class

---
+++ src/arti/fingerprints/core.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+
+from functools import reduce
+from operator import xor
+from typing import Optional
+
+import farmhash
+
+from arti.internal.utils import int64, qname, uint64
+
+
+class Fingerprint:
+    """ Fingerprint represents a unique identity as an int64 value.
+
+        Using an int(64) has a number of convenient properties:
+        - can be combined independent of order with XOR
+        - can be stored relatively cheaply
+        - empty 0 values drop out when combined (5 ^ 0 = 5)
+        - is relatively cross-platform (across databases, languages, etc)
+
+        There are two "special" Fingerprints w/ factory functions that, when combined with other
+        Fingerprints:
+        - `empty()`: returns `empty()`
+        - `identity()`: return the other Fingerprint
+    """
+
+    def __init__(self, key: Optional[int64]):
+        self.key = key
+
+    def combine(self, *others: Fingerprint) -> Fingerprint:
+        return reduce(xor, others, self)
+
+    @classmethod
+    def empty(cls) -> Fingerprint:
+        """ Return a Fingerprint that, when combined, will return Fingerprint.empty()
+        """
+        return cls(None)
+
+    @classmethod
+    def from_int(cls, x: int, /) -> Fingerprint:
+        return cls.from_int64(int64(x))
+
+    @classmethod
+    def from_int64(cls, x: int64, /) -> Fingerprint:
+        return cls(x)
+
+    @classmethod
+    def from_string(cls, x: str, /) -> Fingerprint:
+        """ Fingerprint an arbitrary string.
+
+            Fingerprints using Farmhash Fingerprint64, converted to int64 via two's complement.
+        """
+        return cls.from_uint64(uint64(farmhash.fingerprint64(x)))
+
+    @classmethod
+    def from_uint64(cls, x: uint64, /) -> Fingerprint:
+        return cls.from_int64(int64(x))
+
+    @classmethod
+    def identity(cls) -> Fingerprint:
+        """ Return a Fingerprint that, when combined, will return the other Fingerprint.
+        """
+        return cls(int64(0))
+
+    @property
+    def is_empty(self) -> bool:
+        return self.key is None
+
+    @property
+    def is_identity(self) -> bool:
+        return self.key == 0
+
+    def __repr__(self) -> str:
+        return f"{qname(self)}(key={self.key})"
+
+    __str__ = __repr__
+
+    def __xor__(self, other: Fingerprint) -> Fingerprint:
+        if self.key is None:
+            return Fingerprint.empty()
+        if isinstance(other, Fingerprint):
+            if other.key is None:
+                return Fingerprint.empty()
+            return Fingerprint(self.key ^ other.key)
+        return NotImplemented
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, int):
+            return self.key == other
+        if isinstance(other, Fingerprint):
+            return self.key == other.key
+        return NotImplemented
---
+++ tests/arti/test_fingerprints.py
@@ -0,0 +1,55 @@
+from itertools import permutations
+
+import pytest
+
+from arti.fingerprints.core import Fingerprint
+from arti.internal.utils import int64, uint64
+
+
+def test_Fingerprint() -> None:
+    assert Fingerprint(key=int64(5)).key == 5
+    assert Fingerprint.empty().key is None
+    assert Fingerprint.from_int(-5) == -5
+    assert Fingerprint.from_int64(int64(-5)) == -5
+    assert Fingerprint.from_string("OK") == -7962813320811223369
+    assert Fingerprint.from_string("ok") == 5227454011934222951
+    assert Fingerprint.from_uint64(uint64(5)) == 5
+    assert Fingerprint.from_uint64(uint64(int64(-5))) == -5
+    assert Fingerprint.identity() == 0
+
+    with pytest.raises(ValueError):
+        Fingerprint.from_int(uint64._max)
+
+    f1, f2 = Fingerprint(key=int64(1)), Fingerprint(key=int64(2))
+    assert f1 != "1"
+    assert f1 != f2
+    assert not f1.is_empty
+    assert not f1.is_identity
+    assert Fingerprint.empty().is_empty
+    assert Fingerprint.identity().is_identity
+    assert not Fingerprint.empty().is_identity
+    assert not Fingerprint.identity().is_empty
+
+    assert str(f1) == repr(f1) == "Fingerprint(key=int64(1))"
+
+
+def test_Fingerprint_math() -> None:
+    f1, f2, f3, f4, f5 = [Fingerprint(key=int64(i)) for i in range(5)]
+    # associative
+    assert f1.combine(f2.combine(f3)) == f3.combine(f1.combine(f2))
+    # commutative
+    combined = f1.combine(f2, f3, f4, f5)
+    for permutation in permutations([f1, f2, f3, f4, f5]):
+        head, *tail = permutation
+        assert head.combine(*tail) == combined
+    # identity
+    assert f1.combine(Fingerprint.identity()) == f1
+    # self-inverse
+    assert f1.combine(f1) == 0
+    # empty cascades
+    assert Fingerprint.empty().combine(f1).is_empty
+    assert f1.combine(Fingerprint.empty()).is_empty
+
+    for val in [None, 0, "a"]:
+        with pytest.raises(TypeError):
+            f1.combine(val)  # type: ignore
