16aa627d160fa789d1d6de5fc0cef529eeba65bb
Steven S. Kim <8191272+mikss@users.noreply.github.com>
2021-07-16 11:14:20 -0400

    python types

---
+++ src/arti/types/python.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import Any
+
+from arti.types.core import Int64, Type, TypeAdapter, TypeSystem
+
+python = TypeSystem("python")
+
+
+@python.register_adapter
+class PyInt64(TypeAdapter):
+    external = int
+    internal = Int64
+
+    @classmethod
+    def to_external(cls, type_: Type) -> Any:
+        return cls.external
+
+    @classmethod
+    def to_internal(cls, type_: Any) -> Type:
+        return cls.internal()
