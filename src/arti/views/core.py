3dd710f4903847948bbd4646e51ea5ec3a096f61
Steven S. Kim <8191272+mikss@users.noreply.github.com>
2021-07-20 13:40:22 -0400

    tests for views

--- src/arti/views/core.py
+++ src/arti/views/core.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import ClassVar, Optional
+from typing import Any, ClassVar, Optional
 
 from multimethod import multimethod
 
@@ -8,6 +8,7 @@ from arti.artifacts.core import Artifact
 from arti.formats.core import Format
 from arti.internal.models import Model
 from arti.storage.core import Storage
+from arti.types.core import Type, TypeSystem
 
 
 class View(Model):
@@ -17,29 +18,28 @@ class View(Model):
     """
 
     __abstract__ = True
-    type_system: ClassVar[TypeSystem]
-
+    type_system: ClassVar[TypeSystem] = None
     data: Optional[Any] = None
 
     # TODO write/read partitioned data, column subset
 
     @classmethod
-    def read(cls, artifact: Artifact):
+    def read(cls, artifact: Artifact) -> View:
         return read(cls(), artifact.format, artifact.storage)
 
-    def write(self, artifact: Artifact):
-        return write(self, artifact.format, artifact.storage)
+    def write(self, artifact: Artifact) -> None:
+        write(self, artifact.format, artifact.storage)
 
-    def validate(self, schema: Optional[Type]) -> None:
+    def validate(self, schema: Optional[Type]) -> View:
         # TODO: eventually producer will validate
         pass
 
 
 @multimethod
-def read(view: View, format: Format, storage: Storage):
+def read(view: View, format: Format, storage: Storage) -> View:
     ...
 
 
 @multimethod
-def write(view: View, format: Format, storage: Storage):
+def write(view: View, format: Format, storage: Storage) -> None:
     ...
