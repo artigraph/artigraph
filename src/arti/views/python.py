2461a6e850ddbd3020bdc7ced2e9c59ebb654bb1
Steven S. Kim <8191272+mikss@users.noreply.github.com>
2021-07-20 13:19:20 -0400

    python view initial pass

---
+++ src/arti/views/python.py
@@ -0,0 +1,25 @@
+import pickle
+from typing import ClassVar
+
+from arti.formats.pickle import Pickle
+from arti.storage.local import LocalFile
+from arti.types.core import TypeSystem
+from arti.types.python import python
+from arti.views.core import View, read, write
+
+
+class Python(View):
+    type_system: ClassVar[TypeSystem] = python
+
+
+@read.register
+def _(view: Python, format: Pickle, storage: LocalFile):
+    with open(storage.path, "rb") as file:
+        view.data = pickle.load(file)
+    return view
+
+
+@write.register
+def _(view: Python, format: Pickle, storage: LocalFile):
+    with open(storage.path, "wb") as file:
+        pickle.dump(view.data, file)
---
+++ tests/arti/views/test_views.py
@@ -0,0 +1,26 @@
+import pickle
+from unittest.mock import mock_open, patch
+
+import pytest
+from pydantic import ValidationError
+
+from arti.artifacts.core import Artifact
+from arti.formats.pickle import Pickle
+from arti.storage.local import LocalFile
+from arti.views.core import View
+from arti.views.python import Python
+
+
+def test_View() -> None:
+    with pytest.raises(ValidationError, match="cannot be instantiated directly"):
+        View()
+
+
+def test_Python_View() -> None:
+    a = Artifact()
+    a.storage = LocalFile(path="/tmp/foo.pkl")
+    a.format = Pickle()
+    with patch("builtins.open", mock_open(read_data=pickle.dumps(1))) as mock_file:
+        view = Python.read(a)
+        mock_file.assert_called_with("/tmp/foo.pkl", "rb")
+        assert view.data == 1
