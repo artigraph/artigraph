ce8479499667315d23d877240e1f51e0100c9cc6
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-07-21 08:32:38 -0500

    Add TypeAdapter.matches_* methods and expand tests (#64)

--- src/arti/types/python.py
+++ src/arti/types/python.py
@@ -4,18 +4,29 @@ from typing import Any
 
 from arti.types.core import Int64, Type, TypeAdapter, TypeSystem
 
-python = TypeSystem(key="python", system_metaclass=True)
+python = TypeSystem(key="python")
 
 
-@python.register_adapter
-class PyInt64(TypeAdapter):
-    external = int
-    internal = Int64
+# Python types are all instances of `type` - there is no "meaningful" metaclass. Hence, we must
+# match on and return the type identity.
+class _SingletonTypeAdapter(TypeAdapter):
+    @classmethod
+    def to_artigraph(cls, type_: Any) -> Type:
+        return cls.artigraph()
 
     @classmethod
-    def to_external(cls, type_: Type) -> Any:
-        return cls.external
+    def matches_system(cls, type_: Any) -> bool:
+        return type_ is cls.system
 
     @classmethod
-    def to_internal(cls, type_: Any) -> Type:
-        return cls.internal()
+    def to_system(cls, type_: Type) -> Any:
+        return cls.system
+
+
+def _gen_adapter(name: str, *, artigraph: type[Type], system: Any) -> type[TypeAdapter]:
+    return python.register_adapter(
+        type(name, (_SingletonTypeAdapter,), {"artigraph": artigraph, "system": system})
+    )
+
+
+_gen_adapter("PyInt64", artigraph=Int64, system=int)
