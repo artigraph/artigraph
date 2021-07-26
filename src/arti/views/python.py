25ab7e1ea3b715afcb2dcbd93164eb032c11773b
Steven S. Kim <8191272+mikss@users.noreply.github.com>
2021-07-26 11:46:11 -0400

    builtin not object

--- src/arti/views/python.py
+++ src/arti/views/python.py
@@ -9,25 +9,25 @@ from arti.types.python import python
 from arti.views.core import View
 
 
-class _PythonObject(View):
+class _PythonBuiltin(View):
     __abstract__ = True
 
     type_system: ClassVar[TypeSystem] = python
 
 
-class Int(_PythonObject):
+class Int(_PythonBuiltin):
     python_type = int
 
 
 @read.register
-def _read_pickle_localfile_python(format: Pickle, storage: LocalFile, view: _PythonObject) -> Any:
+def _read_pickle_localfile_python(format: Pickle, storage: LocalFile, view: _PythonBuiltin) -> Any:
     with open(storage.path, "rb") as file:
         return pickle.load(file)
 
 
 @write.register
 def _write_pickle_localfile_python(
-    data: Any, format: Pickle, storage: LocalFile, view: _PythonObject
+    data: Any, format: Pickle, storage: LocalFile, view: _PythonBuiltin
 ) -> None:
     with open(storage.path, "wb") as file:
         pickle.dump(data, file)
