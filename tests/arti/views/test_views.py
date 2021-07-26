35b4dc74f800e9c2826fa5a5601d70c630e83901
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-07-26 13:39:43 -0400

    Register Views by priority

--- tests/arti/views/test_views.py
+++ tests/arti/views/test_views.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import ClassVar
+
 import pytest
 from pydantic import ValidationError
 
@@ -7,3 +11,21 @@ from arti.views.core import View
 def test_View() -> None:
     with pytest.raises(ValidationError, match="cannot be instantiated directly"):
         View()
+
+
+def test_View_registry() -> None:
+    class V(View):
+        __abstract__ = True
+        _registry_: ClassVar[dict[type, type[View]]] = {}
+
+    class Int(V):
+        python_type = int
+
+    class Int2(V):
+        python_type = int
+        priority = Int.priority + 1
+
+    class Str(V):
+        python_type = str
+
+    assert V._registry_ == {int: Int2, str: Str}
