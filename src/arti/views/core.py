0cb46f7804fa91830fc7b602df2099d88c9d0fd6
Steven Soojin Kim <8191272+mikss@users.noreply.github.com>
2021-07-26 13:35:05 -0400

    re-group attributes of View
    Co-authored-by: Jacob Hayes <jacob.r.hayes@gmail.com>

--- src/arti/views/core.py
+++ src/arti/views/core.py
@@ -14,14 +14,11 @@ class View(Model):
     """
 
     __abstract__ = True
-
     _registry_: ClassVar[dict[type, type[View]]] = dict()
 
-    type_system: ClassVar[TypeSystem]
-
-    python_type: ClassVar[type]
-
     priority: ClassVar[int] = 0  # Set priority of this view for its python_type. Higher is better.
+    python_type: ClassVar[type]
+    type_system: ClassVar[TypeSystem]
 
     def __init_subclass__(cls, **kwargs: Any) -> None:
         super().__init_subclass__(**kwargs)
