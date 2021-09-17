645d7a9c7f4f52ea57c4ef37491fd36cb4a343b3
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-09-17 16:48:54 -0400

    Shorten sgqlc import and expand tests

--- tests/arti/types/test_sgqlc_adapters.py
+++ tests/arti/types/test_sgqlc_adapters.py
@@ -22,7 +22,7 @@ class MyType(st.Type, MyInterface):
 
 class MySuperType(st.Type):
     id = st.non_null(int)
-    value = st.list_of(MyType)
+    value = st.list_of(st.non_null(MyType))
 
 
 @pytest.mark.parametrize(
@@ -39,6 +39,28 @@ def test_sgqlc_scalars(sgqlc_type: type[st.Scalar], arti_type: type[at.Type]) ->
     a = sgqlc_type_system.to_artigraph(sgqlc_type, hints={})
     assert isinstance(a, arti_type)
     assert sgqlc_type_system.to_system(a, hints={}) is sgqlc_type
+
+
+def test_sgqlc_interface_adapter() -> None:
+    arti_iface = sgqlc_type_system.to_artigraph(MyInterface, hints={})
+    assert isinstance(arti_iface, at.Struct)
+    sgqlc_iface = sgqlc_type_system.to_system(arti_iface, hints={"sgqlc.abstract": True})
+    assert issubclass(sgqlc_iface, st.Interface)
+
+    for k, v in MyInterface._ContainerTypeMeta__fields.items():
+        assert k in sgqlc_iface._ContainerTypeMeta__fields
+        # these are st.BaseMeta types
+        assert v.type == sgqlc_iface._ContainerTypeMeta__fields[k].type
+
+
+def test_sgqlc_type_adapter() -> None:
+    arti_type = sgqlc_type_system.to_artigraph(MyType, hints={})
+    assert isinstance(arti_type, at.Struct)
+    assert list(arti_type.fields.keys()) == [field.name for field in MyType]
+    sgqlc_type = sgqlc_type_system.to_system(arti_type, hints={"sgqlc.interfaces": (MyInterface,)})
+    assert issubclass(sgqlc_type, st.Type)
+    # check that the MyType interface(s) got passed around
+    assert sgqlc_type.__interfaces__ == (MyInterface,)
 
 
 def test_nested_sgqlc_type() -> None:
@@ -65,21 +87,27 @@ def test_nested_sgqlc_type() -> None:
     assert issubclass(s.value.type, st.Type)
 
     # value arti
-    a_sub = a.fields["value"].value_type
-    assert a_sub.name == MyType.__name__
-    assert a_sub.nullable
-    assert set(a_sub.fields) == {"a", "b", "c"}
-    assert isinstance(a_sub.fields["a"], at._Int)
-    assert isinstance(a_sub.fields["b"], at.String)
-    assert isinstance(a_sub.fields["c"], at._Int)
+    a_container = a.fields["value"]
+    assert isinstance(a_container, at.List)
+    assert a_container.nullable
+    a_value = a_container.value_type
+    assert isinstance(a_value, at.Struct)
+    assert a_value.name == MyType.__name__
+    assert not a_value.nullable
+    assert set(a_value.fields) == {"a", "b", "c"}
+    assert isinstance(a_value.fields["a"], at._Int)
+    assert isinstance(a_value.fields["b"], at.String)
+    assert isinstance(a_value.fields["c"], at._Int)
     # value sgqlc
-    s_sub = s.value.type
-    assert s_sub.__name__ == f"[{MyType.__name__}]"
-    assert is_sgqlc_nullable(s_sub)
-    assert set(f.name for f in s_sub) == {"a", "b", "c"}
-    assert issubclass(s_sub.a.type, st.Int)
-    assert issubclass(s_sub.b.type, st.String)
-    assert issubclass(s_sub.c.type, st.Int)
+    s_container = s.value.type
+    assert s_container.__name__ == f"[{MyType.__name__}!]"
+    assert is_sgqlc_nullable(s_container)
+    s_value = s_container.mro()[1]
+    assert not is_sgqlc_nullable(s_value)
+    assert set(f.name for f in s_container) == {"a", "b", "c"}
+    assert issubclass(s_container.a.type, st.Int)
+    assert issubclass(s_container.b.type, st.String)
+    assert issubclass(s_container.c.type, st.Int)
 
 
 def test_sgqlc_enum_adapter() -> None:
@@ -166,25 +194,3 @@ def test_sgqlc_non_null_list_of_non_null_adapting() -> None:
     assert sgqlc_t.__name__.endswith("!")
     assert sgqlc_t.mro()[1].__name__.startswith("[")
     assert sgqlc_t.mro()[1].__name__.endswith("!]")
-
-
-def test_sgqlc_interface_adapter() -> None:
-    arti_iface = sgqlc_type_system.to_artigraph(MyInterface, hints={})
-    assert isinstance(arti_iface, at.Struct)
-    sgqlc_iface = sgqlc_type_system.to_system(arti_iface, hints={"sgqlc.abstract": True})
-    assert issubclass(sgqlc_iface, st.Interface)
-
-    for k, v in MyInterface._ContainerTypeMeta__fields.items():
-        assert k in sgqlc_iface._ContainerTypeMeta__fields
-        # these are st.BaseMeta types
-        assert v.type == sgqlc_iface._ContainerTypeMeta__fields[k].type
-
-
-def test_sgqlc_type_adapter() -> None:
-    arti_type = sgqlc_type_system.to_artigraph(MyType, hints={})
-    assert isinstance(arti_type, at.Struct)
-    assert list(arti_type.fields.keys()) == [field.name for field in MyType]
-    sgqlc_type = sgqlc_type_system.to_system(arti_type, hints={"sgqlc.interfaces": (MyInterface,)})
-    assert issubclass(sgqlc_type, st.Type)
-    # check that the MyType interface(s) got passed around
-    assert sgqlc_type.__interfaces__ == (MyInterface,)
