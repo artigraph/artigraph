761e8dd6277640a46213dbf45c2f1e255d5b1a93
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-09-29 20:10:48 -0400

    Add wrap_exc helper to ease chaining error context

--- tests/arti/internal/test_internal.py
+++ tests/arti/internal/test_internal.py
@@ -1,5 +1,25 @@
-from arti.internal import version
+import pytest
+
+from arti.internal import version, wrap_exc
 
 
 def test_version() -> None:
     assert version is not None
+
+
+def test_wrap_exc() -> None:
+    root_error = ValueError("test")
+    with pytest.raises(ValueError, match="a - test") as exc:
+        with wrap_exc(ValueError, prefix="a"):
+            raise root_error
+    assert exc.value.__cause__ is root_error
+
+    with pytest.raises(ValueError, match="ab - test") as exc:
+        with wrap_exc(ValueError, prefix="a"):
+            with wrap_exc(ValueError, prefix="b"):
+                raise root_error
+    assert exc.value.__cause__ is root_error
+
+    with wrap_exc(ValueError, prefix="shouldn't run"):
+        x = 5
+    assert x == 5
