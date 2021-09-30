a5a378a3fcf0585858321e01cc306c14e06a4f0f
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-09-29 20:10:48 -0400

    Add wrap_exc helper to ease chaining error context

--- src/arti/internal/__init__.py
+++ src/arti/internal/__init__.py
@@ -1,3 +1,27 @@
 import importlib.metadata
+from collections.abc import Iterator
+from contextlib import contextmanager
 
 version = importlib.metadata.version("arti")
+
+
+@contextmanager
+def wrap_exc(error_type: type[Exception], *, prefix: str) -> Iterator[None]:
+    """Wrap exceptions of `error_type` and add a message prefix.
+
+    `error_type` must be initializable with a single string message argument.
+
+    NOTE: When used inside a generator, any exceptions raised by the *caller of the generator* will **not** be wrapped.
+    """
+    try:
+        yield
+    except error_type as e:
+        msg = str(e)
+        if getattr(e, "wrapped", False) and e.__cause__ is not None:
+            src = e.__cause__  # Shorten exception chains to the root and last wrapped only
+        else:
+            msg = f" - {msg}"
+            src = e
+        error = error_type(f"{prefix}{msg}")
+        error.wrapped = True  # type: ignore
+        raise error from src
