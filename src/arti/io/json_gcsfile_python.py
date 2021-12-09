2a7ebf2005f5b2c473810e9a27ba282e7b0a80cc
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-12-09 08:41:36 -0500

    Add GCSFile Storage
    Signed-off-by: Jacob Hayes <jacob.r.hayes@gmail.com>

---
+++ src/arti/io/json_gcsfile_python.py
@@ -0,0 +1,48 @@
+import json
+from collections.abc import Sequence
+from itertools import chain
+from typing import Any
+
+from gcsfs import GCSFileSystem
+
+from arti.formats.json import JSON
+from arti.io import register_reader, register_writer
+from arti.storage.google.cloud.storage import GCSFilePartition
+from arti.types import Collection, Type
+from arti.views.python import PythonBuiltin
+
+# TODO: Do I need to inject the partition keys into the returned data? Likely useful...
+# Maybe a View option?
+
+
+def _read_json_file(path: str) -> Any:
+    # TODO: GCSFileSystem needs to be injected somehow
+    with GCSFileSystem().open(path, "r") as file:
+        return json.load(file)
+
+
+@register_reader
+def _read_json_gcsfile_python(
+    type_: Type,
+    format: JSON,
+    storage_partitions: Sequence[GCSFilePartition],
+    view: PythonBuiltin,
+) -> Any:
+    if isinstance(type_, Collection) and type_.is_partitioned:
+        return list(
+            chain.from_iterable(
+                _read_json_file(storage_partition.qualified_path)
+                for storage_partition in storage_partitions
+            )
+        )
+    else:
+        assert len(storage_partitions) == 1  # Better error handled in base read
+        return _read_json_file(storage_partitions[0].qualified_path)
+
+
+@register_writer
+def _write_json_gcsfile_python(
+    data: Any, type_: Type, format: JSON, storage_partition: GCSFilePartition, view: PythonBuiltin
+) -> None:
+    with GCSFileSystem().open(storage_partition.qualified_path, "w") as file:
+        json.dump(data, file)
---
+++ src/arti/storage/google/cloud/storage.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+from gcsfs import GCSFileSystem
+
+from arti import Fingerprint, InputFingerprints, Storage, StoragePartition
+from arti.internal.models import Model
+from arti.storage._internal import parse_spec, spec_to_wildcard
+
+
+class _GCSMixin(Model):
+    bucket: str
+    path: str
+
+    @property
+    def qualified_path(self) -> str:
+        return f"{self.bucket}/{self.path}"
+
+
+class GCSFilePartition(_GCSMixin, StoragePartition):
+    def compute_content_fingerprint(self) -> Fingerprint:
+        # TODO: GCSFileSystem needs to be injected somehow
+        info = GCSFileSystem().info(f"{self.bucket}/{self.path}")
+        # Prefer md5Hash if available
+        return Fingerprint.from_string(info["md5Hash"] if "md5Hash" in info else info["crc32c"])
+
+
+class GCSFile(_GCSMixin, Storage[GCSFilePartition]):
+    path = str(
+        Path("{graph_name}")
+        / "{path_tags}"
+        / "{names}"
+        / "{partition_key_spec}"
+        / "{input_fingerprint}"
+        / "{name}.{extension}"
+    )
+
+    def discover_partitions(
+        self, input_fingerprints: InputFingerprints = InputFingerprints()
+    ) -> tuple[GCSFilePartition, ...]:
+        # NOTE: The bucket/path must *already* have any graph tags resolved, otherwise they will be try to be parsed as
+        # partition keys.
+        spec = f"{self.bucket}/{self.path}"
+        wildcard = spec_to_wildcard(spec, self.key_types)
+        # TODO: GCSFileSystem needs to be injected somehow
+        paths = GCSFileSystem().glob(wildcard)
+        path_metadata = parse_spec(
+            paths, spec=spec, key_types=self.key_types, input_fingerprints=input_fingerprints
+        )
+        return tuple(
+            self.generate_partition(input_fingerprint=input_fingerprint, keys=keys)
+            for (bucket, path), (input_fingerprint, keys) in {
+                tuple(path.split("/", 1)): metadata for path, metadata in path_metadata.items()
+            }.items()
+        )
