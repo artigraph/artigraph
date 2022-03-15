962863eb456cd5854d0aa7ef482ed5b5effd56e3
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-03-14 22:42:52 -0400

    Download and run fake-gcs-server directly in tests
    Signed-off-by: Jacob Hayes <jacob.r.hayes@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
+import platform
+import shutil
 import time
 from collections.abc import Generator
+from pathlib import Path
 from unittest import mock
 
 import gcsfs
@@ -8,32 +11,49 @@ import pytest
 import requests
 import sh
 
+MACHINE_MAP = {
+    "aarch64": "arm64",
+    "arm64": "arm64",
+    "x86_64": "amd64",
+}
+BIN_CACHE_DIR = Path(__file__).parent / ".bin_cache"
+BIN_MACHINE = MACHINE_MAP[platform.machine()]
+BIN_SYSTEM = platform.system()
+
+
+def _get_fake_gcs_server_cmd(
+    machine: str = BIN_MACHINE, system: str = BIN_SYSTEM, version: str = "1.37.0"
+) -> sh.Command:
+    binpath = BIN_CACHE_DIR / f"fake-gcs-server-{version}-{system}-{machine}"
+    if not binpath.exists():
+        tgz_name = f"{binpath}.tgz"
+        url = f"https://github.com/fsouza/fake-gcs-server/releases/download/v{version}/fake-gcs-server_{version}_{system}_{machine}.tar.gz"
+        with requests.get(url, stream=True) as resp:
+            if resp.status_code != requests.codes.ok:
+                pytest.skip(f"fake-gcs-server for {system} {machine} is not available")
+            with open(tgz_name, "wb") as tgz:
+                shutil.copyfileobj(resp.raw, tgz)
+        sh.tar("-xf", tgz_name, "fake-gcs-server")
+        sh.mv("fake-gcs-server", binpath)
+        sh.rm(tgz_name)
+    return sh.Command(binpath)
+
 
 # For now, run a single emulator per session. Tests should use separate buckets for isolation.
-#
-# Modified from https://github.com/fsspec/gcsfs/blob/2022.02.0/gcsfs/tests/conftest.py#L58
 @pytest.fixture(scope="session")
-def gcs_emulator(
-    fake_gcs_server_version: str = "1.37.0", port: int = 2784
-) -> Generator[str, None, None]:
+def gcs_emulator(port: int = 2784) -> Generator[str, None, None]:
     if "STORAGE_EMULATOR_HOST" in os.environ:
         # assume using real API or otherwise have a server already set up
         yield os.environ["STORAGE_EMULATOR_HOST"]
         return
-    if not sh.which("docker"):
-        raise pytest.skip("docker not available to run fake-gcs-server")
-    container = "arti-test-gcs-emulator"
     url = f"http://localhost:{port}"
-    sh.docker.run(
-        "-d",
-        f"--name={container}",
-        f"-p={port}:{port}",
-        f"fsouza/fake-gcs-server:{fake_gcs_server_version}",
+    fake_gcs_server_proc = _get_fake_gcs_server_cmd()(
         "-backend=memory",
         "-scheme=http",
         f"-external-url={url}",
         f"-port={port}",
         f"-public-host={url}",
+        _bg=True,
     )
     try:
         time.sleep(0.25)
@@ -41,7 +61,8 @@ def gcs_emulator(
         with mock.patch.dict(os.environ, {"STORAGE_EMULATOR_HOST": url}):
             yield url
     finally:
-        sh.docker.rm("-fv", container)
+        fake_gcs_server_proc.terminate()
+        fake_gcs_server_proc.wait()
 
 
 @pytest.fixture()
