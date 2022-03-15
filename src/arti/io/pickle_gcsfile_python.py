5d94004eaa5102c6a74f19044653e74815a4c8e8
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-03-14 23:45:15 -0400

    Add GCSFile Storage (#149)

---
+++ src/arti/io/pickle_gcsfile_python.py
@@ -0,0 +1,48 @@
+import pickle
+from collections.abc import Sequence
+from itertools import chain
+from typing import Any
+
+from gcsfs import GCSFileSystem
+
+from arti.formats.pickle import Pickle
+from arti.io import register_reader, register_writer
+from arti.storage.google.cloud.storage import GCSFilePartition
+from arti.types import Collection, Type
+from arti.views.python import PythonBuiltin
+
+# TODO: Do I need to inject the partition keys into the returned data? Likely useful...
+# Maybe a View option?
+
+
+def _read_pickle_file(path: str) -> Any:
+    # TODO: GCSFileSystem needs to be injected somehow
+    with GCSFileSystem().open(path, "rb") as file:
+        return pickle.load(file)  # nosec # User opted into pickle, ignore bandit check
+
+
+@register_reader
+def _read_pickle_gcsfile_python(
+    type_: Type,
+    format: Pickle,
+    storage_partitions: Sequence[GCSFilePartition],
+    view: PythonBuiltin,
+) -> Any:
+    if isinstance(type_, Collection) and type_.is_partitioned:
+        return list(
+            chain.from_iterable(
+                _read_pickle_file(storage_partition.qualified_path)
+                for storage_partition in storage_partitions
+            )
+        )
+    else:
+        assert len(storage_partitions) == 1  # Better error handled in base read
+        return _read_pickle_file(storage_partitions[0].qualified_path)
+
+
+@register_writer
+def _write_pickle_gcsfile_python(
+    data: Any, type_: Type, format: Pickle, storage_partition: GCSFilePartition, view: PythonBuiltin
+) -> None:
+    with GCSFileSystem().open(storage_partition.qualified_path, "wb") as file:
+        pickle.dump(data, file)
---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import os
+from collections.abc import Generator
+from unittest import mock
+
+import gcp_storage_emulator.server
+import gcsfs
+import pytest
+
+
+# For now, run a single emulator per session. Tests should use separate buckets for isolation.
+@pytest.fixture(scope="session")
+def gcs_emulator() -> Generator[tuple[str, int], None, None]:
+    # port=0 -> run on a random open port; though we have to lookup later.
+    server = gcp_storage_emulator.server.create_server("localhost", 0, in_memory=True)
+    server.start()
+    try:
+        host, port = server._api._httpd.socket.getsockname()
+        with mock.patch.dict(os.environ, {"STORAGE_EMULATOR_HOST": f"http://{host}:{port}"}):
+            yield host, port
+    finally:
+        server.stop()
+
+
+@pytest.fixture()
+def gcs(gcs_emulator: tuple[str, int]) -> gcsfs.GCSFileSystem:
+    return gcsfs.GCSFileSystem()
+
+
+@pytest.fixture()
+def gcs_bucket(
+    request: pytest.FixtureRequest, gcs: gcsfs.GCSFileSystem
+) -> Generator[str, None, None]:
+    # Strip malformed characters (eg: parametrized test names include brackets). Depending on
+    # parametrize inputs, we may have to replace/escape more things.
+    bucket = request.node.name.replace("[", "").replace("]", "")
+    gcs.mkdir(bucket)
+    try:
+        yield bucket
+    finally:
+        gcs.rm(bucket, recursive=True)
