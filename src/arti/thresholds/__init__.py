f0f16b170e97da605337a16e229c1c3a60c6676b
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-09-13 11:07:22 +0200

    Fix Threshold to be a Model
    Signed-off-by: Jacob Hayes <jacob.r.hayes@gmail.com>

--- src/arti/thresholds/__init__.py
+++ src/arti/thresholds/__init__.py
@@ -2,10 +2,11 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 from typing import Any, ClassVar
 
+from arti.internal.models import Model
 from arti.types import Type
 
 
-class Threshold:
+class Threshold(Model):
     type: ClassVar[type[Type]]
 
     def check(self, value: Any) -> bool:
